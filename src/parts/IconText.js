bebcf15997c9897bf678ee1b0dd0cff7eda7a42f
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-01-19 16:32:10 +0800

    create Brand Icon Component

---
+++ src/parts/IconText.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Button from 'elements/Button'
+
+export default function IconText() {
+  return (
+    <Button className='brand-text-icon' type='link' href="">
+        Stay<span className='text-gray-900'>cation.</span>
+        
+    </Button>
+  )
+}
+
