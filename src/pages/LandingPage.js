a1082d7dadb3d14ac57f03ce8e3947939c133b1b
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-03-13 23:35:17 +0800

    create testimony component

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -5,6 +5,7 @@ import MostPicked from "parts/MostPicked";
 
 import landingPage from "json/landingPage.json";
 import Categories from "parts/Categories";
+import Testimoni from "parts/Testimoni";
 
 export default class LandingPage extends Component {
   constructor(props) {
@@ -22,6 +23,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimoni data={landingPage.testimonial} />
       </div>
     );
   }
