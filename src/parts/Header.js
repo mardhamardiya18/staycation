a7ae3d6bb34bed61b3eea9a588579c248cd39a2b
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-04-07 23:13:18 +0800

    create react-reveal & bit transition each section

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useLocation } from 'react-router-dom'
 import Button from 'elements/Button'
 import BrandIcon from 'parts/IconText'
+import { Fade } from 'react-reveal';
 
 export default function Header() {
     const location = useLocation();
@@ -10,29 +11,31 @@ export default function Header() {
     }
 
     return (
-        <header className='spacing-sm'>
-            <div className="container">
-                <nav className="navbar navbar-expand-lg navbar-light">
-                    <BrandIcon />
+        <Fade>
+            <header className='spacing-sm'>
+                <div className="container">
+                    <nav className="navbar navbar-expand-lg navbar-light">
+                        <BrandIcon />
 
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ml-auto">
-                            <li className={`nav-item${getNavLinkClass("/")}`}>
-                                <Button className='nav-link' type='link' href='/'>Home</Button>
-                            </li>
-                            <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
-                                <Button className='nav-link' type='link' href='/browse-by'>Browse By</Button>
-                            </li>
-                            <li className={`nav-item${getNavLinkClass("/stories")}`}>
-                                <Button className='nav-link' type='link' href='/stories'>Stories</Button>
-                            </li>
-                            <li className={`nav-item${getNavLinkClass("/agents")}`}>
-                                <Button className='nav-link' type='link' href='/agents'>Agents</Button>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-        </header>
+                        <div className="collapse navbar-collapse">
+                            <ul className="navbar-nav ml-auto">
+                                <li className={`nav-item${getNavLinkClass("/")}`}>
+                                    <Button className='nav-link' type='link' href='/'>Home</Button>
+                                </li>
+                                <li className={`nav-item${getNavLinkClass("/browse-by")}`}>
+                                    <Button className='nav-link' type='link' href='/browse-by'>Browse By</Button>
+                                </li>
+                                <li className={`nav-item${getNavLinkClass("/stories")}`}>
+                                    <Button className='nav-link' type='link' href='/stories'>Stories</Button>
+                                </li>
+                                <li className={`nav-item${getNavLinkClass("/agents")}`}>
+                                    <Button className='nav-link' type='link' href='/agents'>Agents</Button>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </header>
+        </Fade>
     )
 }
