909a2e4c1318d8a87fdd321d8f1f3d7f438cfb6c
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-04-13 11:49:38 +0800

    create InputNumber & InputDate component

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,93 @@
+import React, { useEffect, useState } from "react";
+
+import propTypes from "prop-types";
+
+import "./index.scss";
+import { DateRange } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+
+import formatDate from "util/formatDate";
+import iconCalendar from "../../../../src/assets/images/icons/ic_calendar.svg";
+
+const Date = (props) => {
+  const { value, placeholder, name } = props;
+  const { isShowed, setIsShowed } = useState(false);
+
+  const datePickerChange = (value) => {
+    const target = {
+      target: {
+        value: value.selection,
+        name: name,
+      },
+    };
+
+    props.onChange(target);
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  });
+
+  const refDate = useRef(null);
+  const handleClickOutside = (event) => {
+    if (refDate && !refDate.current.contains(event.target)) {
+      setIsShowed(false);
+    }
+  };
+
+  const check = (focus) => {
+    focus.indexOf(1) < 0 && setIsShowed(false);
+  };
+
+  const displayDate = `${value.startDate ? formatDate(value.startDate) : ""}${
+    value.endDate ? " - " + formatDate(value.endDate) : ""
+  }`;
+
+  return (
+    <div
+      ref={refDate}
+      className={["input-date mb-3", props.outerClassName].join(" ")}
+    >
+      <div className="input-group">
+        <div className="input group prepend bg-gray-900">
+          <span className="input-group-text">
+            <img src={iconCalendar} alt="" />
+          </span>
+        </div>
+        <input
+          readOnly
+          value={displayDate}
+          placeholder={placeholder}
+          type="text"
+          className="form-control"
+          onClick={() => setIsShowed(!isShowed)}
+        />
+        {isShowed && (
+          <div className="date-range-wrapper">
+            <DateRange
+              editableDateInputs={true}
+              onChange={datePickerChange}
+              moveRangeOnFirstSelection={false}
+              onRangeFocusChange={check}
+              ranges={[value]}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+Date.propTypes = {
+  value: propTypes.oneOfType([propTypes.string, propTypes.number]),
+  onChange: propTypes.func,
+  placeholder: propTypes.string,
+  outerClassName: propTypes.string,
+};
+
+export default index;
